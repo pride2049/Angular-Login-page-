b287c7776f23ea78ddf5acd4f3ba6f23bcd2d0ab
Eduardo <105794732+pride2049@users.noreply.github.com>
2025-06-13 19:06:22 +0000

    Initial commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../component/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../component/primary-input/primary-input.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [DefaultLoginLayoutComponent,
+            ReactiveFormsModule,
+            PrimaryInputComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+}
